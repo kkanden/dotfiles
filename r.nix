0eb70e884a237115b80f5779be62929a917e52c5
oliwia <24637207+kkanden@users.noreply.github.com>
2025-03-30 22:58:39 +0200

    home: add r.nix

--- r.nix
+++ r.nix
@@ -1,44 +1,8 @@
-{pkgs, ...}: let
-  overlay1 = final: prev: let
-    bslib = prev.rPackages.buildRPackage {
-      name = "bslib";
-      src = prev.fetchFromGitHub {
-        owner = "rstudio";
-        repo = "bslib";
-        rev = "ee34398b1d93056c302560512a090c1326aff7cf";
-        sha256 = "18vwabri5fxl3hkmy02cmidy8vbiqvglnnxiqg9ibg5s141j63vq";
-      };
-      propagatedBuildInputs = with prev.rPackages; [
-        base64enc
-        cachem
-        fastmap
-        htmltools
-        jquerylib
-        jsonlite
-        lifecycle
-        memoise
-        mime
-        rlang
-        sass
-      ];
-    };
-  in {
-    rPackages = prev.rPackages // {inherit bslib;};
-  };
-  pkgs_r = pkgs.extend overlay1;
-  r-packages = with pkgs_r.rPackages;
+{ pkgs, ... }:
+let
+  r-packages =
+    with pkgs.rPackages;
     [
-      base64enc
-      cachem
-      fastmap
-      htmltools
-      jquerylib
-      jsonlite
-      lifecycle
-      memoise
-      mime
-      rlang
-      sass
       languageserver
       data_table
       tidyverse
@@ -52,10 +16,14 @@
       plotly
       shinytitle
       RPostgres
+      gt
+      pacman
+      ellipse
     ]
-    ++ [pkgs.rPackages.config]; # have to separate to avoid conflict with variable;
-in {
+    ++ [ pkgs.rPackages.config ]; # have to separate to avoid conflict with variable;
+in
+{
   home.packages = [
-    (pkgs.rWrapper.override {packages = r-packages;}) # R
+    (pkgs.rWrapper.override { packages = r-packages; }) # R
   ];
 }
